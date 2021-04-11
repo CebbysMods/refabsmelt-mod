77fa5eed0db174306b81631e400b69179d0a9013
JanisValentinovics <valentinovicskristians@gmail.com>
2021-04-11 12:47:39 +0300

    Initial file upload

---
+++ src/main/java/lv/cebbys/refabsmelt/Refabsmelt.java
@@ -0,0 +1,12 @@
+package lv.cebbys.refabsmelt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Refabsmelt implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		
+	}
+
+}
